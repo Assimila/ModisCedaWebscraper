0794cb29ea9103ebe59fc3ed652093a95d855b1e
JamesSalter <james.salter@assimila.eu>
2020-04-24 14:49:04 +0100

    MODIS data webscraping. Exception handling and testing to be included

--- Webscraper_BASH_method.py
+++ Webscraper_BASH_method.py
@@ -131,18 +131,4 @@ class Webscraper:
 
 
 
-            # bash_command = "curl" + url + " -o  " + file_name + \
-                                                     #"-H
-    # 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'Referer:" + url + " -H 'Cookie: ceda.session.2=807e74fa28188fb1e212991f168469daba2bb0724de480b9faf6342d78b7c2007cccc805bb0a4bd3bf0ec767cb376fc0-20a4d69a30e0f6b4a752df32e472bf4d-2a50ab135173f3e4d3d9fcb878ce1649c9fdd808d889a3f8cd9b84119c4f4cf4; ceda.session.1=f9f37f4f0121a4bb5b599e3beb77529b74663c7e60ed9f82d24ba5ad554288c7-d3ea7e8b348a0c9e5d319fcb8c1a21f1-4c83252e4e594bcd3f3f92e10e215a96b78f3e8f1722cf73da3053491f9cc9a1; auth_tkt="ab08c77d35928df334e1ce6781f04e8c5e847839jlewis005!{\"userkey\": \"35335\"\054 \"accountid\": \"NotAssigned\"}"' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1'"
-        #bash_command = "curl " \
-                       #"'http://dap.ceda.ac.uk/thredds/fileServer" \
-                       #"/neodc/modis/data/MOD11A1/collection6/2020
-            # /01" \
-        #bash_command = "curl
-        #'http://dap.ceda.ac.uk/thredds/fileServer/neodc/modis/data' \
-        #'/MOD11A1/collection6/2020/01/16/MOD11A1.A2020016.h01v08.006' \
-        #'.2020018001036.hdf' -o
-        #MOD11A1.A2020016.h01v08.006.2020018001036.hdf -H
-        # 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'Referer: http://data.ceda.ac.uk/neodc/modis/data/MOD11A1/collection6/2020/01/16' -H 'Cookie: ceda.session.2=807e74fa28188fb1e212991f168469daba2bb0724de480b9faf6342d78b7c2007cccc805bb0a4bd3bf0ec767cb376fc0-20a4d69a30e0f6b4a752df32e472bf4d-2a50ab135173f3e4d3d9fcb878ce1649c9fdd808d889a3f8cd9b84119c4f4cf4; ceda.session.1=f9f37f4f0121a4bb5b599e3beb77529b74663c7e60ed9f82d24ba5ad554288c7-d3ea7e8b348a0c9e5d319fcb8c1a21f1-4c83252e4e594bcd3f3f92e10e215a96b78f3e8f1722cf73da3053491f9cc9a1; auth_tkt='ab08c77d35928df334e1ce6781f04e8c5e847839jlewis005!{\'userkey\': \'35335\''\054 \'accountid\': \'NotAssigned\'}'' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1'"
-        # wget.download(self.urllist[0], '/workspace/webscraping/data')
 
